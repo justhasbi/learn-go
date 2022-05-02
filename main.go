1de5825cd3311dbfbc36db861ed6e678892757e0
Hasbi Shuhada <hasbishuhada@Hasbis-MacBook-Pro.local>
2022-05-02 08:36:50 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	
+	// golang data type
+	fmt.Println("satu = ", 1)
+	fmt.Println("dua = ", 2)
+	fmt.Println("tiga koma lima = ", 3.5)
+	
+	fmt.Println("False = ", false)
+	fmt.Println("True = ", true)
+
+	fmt.Println("Hasbi")
+	fmt.Println("Hasbi Shuhada")
+	fmt.Println(len("hasbishuhada"))
+	fmt.Println("hasbi"[0])
+
+	// golang variable
+	var name string = "hasbi"
+	name = "hasbi shuhada"
+	fmt.Println(name)
+}
